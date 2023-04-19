d2a0696d01ccb3e0a68a734a3ba9c45ab7fba5ed
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-19 16:51:43 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+
+ 
+
 const fs = require("fs");
 const express = require("express");
 const app = express();
@@ -8,7 +11,30 @@ const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
 // Middlewares
 app.use(express.json());
 
-// Write GET endpoint for sending all the products to client here
-// Endpoint - /api/v1/products
+// GET endpoint for sending all the products to client
+app.get("/api/v1/products", (req, res) => {
+  console.log(products);
+  res.status(200).json({
+    status: "success",
+    message: "Product fetched successfully",
+    data: {
+      products,
+    },
+  });
+});
 
 module.exports = app;
+// const fs = require("fs");
+// const express = require("express");
+// const app = express();
+
+// // Importing products from products.json file
+// const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
+
+// // Middlewares
+// app.use(express.json());
+
+// // Write GET endpoint for sending all the products to client here
+// // Endpoint - /api/v1/products
+
+// module.exports = app;
